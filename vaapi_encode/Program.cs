ceaebbeb571da0bc9b6fbd9a0922093cb42307dc
SeongTae Jeong <techsharer@outlook.com>
2022-04-11 16:27:35 +0900

    Add C# port of vaapi_encode, vaapi_tarnscode

---
+++ vaapi_encode/Program.cs
@@ -0,0 +1,283 @@
+﻿#define USE_CUDA
+
+using FFmpeg.AutoGen;
+using FFmpeg.AutoGen.Example;
+using System;
+using System.IO;
+
+namespace vaapi_encode
+{
+    /* ffmpeg -hwaccels
+Hardware acceleration methods:
+cuda
+dxva2
+d3d11va
+opencl    
+    */
+    public unsafe class Program
+    {
+        static int width, height;
+
+        static unsafe int set_hwframe_ctx(AVCodecContext* ctx, AVBufferRef* hw_device_ctx)
+        {
+            AVBufferRef* hw_frames_ref;
+            AVHWFramesContext* frames_ctx = null;
+            int err = 0;
+
+            if ((hw_frames_ref = ffmpeg.av_hwframe_ctx_alloc(hw_device_ctx)) == null)
+            {
+#if USE_CUDA
+                Console.WriteLine("Failed to create CUDA frame context.");
+#else
+                Console.WriteLine("Failed to create VAAPI frame context.");
+#endif
+                return -1;
+            }
+
+            frames_ctx = (AVHWFramesContext*)(hw_frames_ref->data);
+#if USE_CUDA
+            frames_ctx->format = AVPixelFormat.AV_PIX_FMT_CUDA;
+#else
+            frames_ctx->format = AVPixelFormat.AV_PIX_FMT_VAAPI;
+#endif
+            frames_ctx->sw_format = AVPixelFormat.AV_PIX_FMT_NV12;
+            frames_ctx->width = width; 
+            frames_ctx->height = height;
+            frames_ctx->initial_pool_size = 20;
+            if ((err = ffmpeg.av_hwframe_ctx_init(hw_frames_ref)) < 0)
+            {
+                Console.WriteLine($"Failed to initialize VAAPI frame context. Error code: {FFmpegHelper.av_err2str(err)}");
+                ffmpeg.av_buffer_unref(&hw_frames_ref);
+                return err;
+            }
+
+            ctx->hw_frames_ctx = ffmpeg.av_buffer_ref(hw_frames_ref);
+            if (ctx->hw_frames_ctx == null)
+            {
+                err = ffmpeg.AVERROR(ffmpeg.ENOMEM);
+            }
+
+            ffmpeg.av_buffer_unref(&hw_frames_ref);
+            return err;
+        }
+
+        static unsafe int encode_write(AVCodecContext* avctx, AVFrame* frame, FileStream fs)
+        {
+            int ret = 0;
+            AVPacket* enc_pkt;
+
+            if ((enc_pkt = ffmpeg.av_packet_alloc()) == null)
+            {
+                return ffmpeg.AVERROR(ffmpeg.ENOMEM);
+            }
+
+            if ((ret = ffmpeg.avcodec_send_frame(avctx, frame)) < 0)
+            {
+                Console.WriteLine($"Error code: {FFmpegHelper.av_err2str(ret)}");
+                goto end;
+            }
+
+            while (true)
+            {
+                ret = ffmpeg.avcodec_receive_packet(avctx, enc_pkt);
+                if (ret != 0)
+                {
+                    break;
+                }
+
+                enc_pkt->stream_index = 0;
+                ReadOnlySpan<byte> data = new ReadOnlySpan<byte>(enc_pkt->data, enc_pkt->size);
+                fs.Write(data);
+                ffmpeg.av_packet_unref(enc_pkt);
+            }
+
+        end:
+            ffmpeg.av_packet_free(&enc_pkt);
+
+            if (ret == ffmpeg.AVERROR_EOF)
+            {
+                return ret;
+            }
+
+            ret = (ret == ffmpeg.AVERROR(ffmpeg.EAGAIN)) ? 0 : -1;
+            return ret;
+        }
+
+        static unsafe int Main(string[] args)
+        {
+            FFmpegBinariesHelper.RegisterFFmpegBinaries();
+
+#if DEBUG
+            Console.WriteLine("Current directory: " + Environment.CurrentDirectory);
+            Console.WriteLine("Running in {0}-bit mode.", Environment.Is64BitProcess ? "64" : "32");
+            Console.WriteLine($"FFmpeg version info: {ffmpeg.av_version_info()}");
+            Console.WriteLine();
+#endif
+
+            int size, err;
+            AVFrame* sw_frame = null, hw_frame = null;
+            AVCodecContext* avctx = null;
+            AVCodec* codec = null;
+#if USE_CUDA
+            string enc_name = "h264_nvenc";
+#else
+            string enc_name = "h264_vaapi";
+#endif
+            AVBufferRef* hw_device_ctx = null;
+
+            width = 1920;
+            height = 1080;
+            size = width * height;
+
+            string dirPath = Path.GetDirectoryName(typeof(Program).Assembly.Location) ?? "";
+
+            // You can get test.dat by running "hw_decode" sample
+            string input_filename = Path.Combine(dirPath, "..", "..", "..", "hw_decode", "bin", "Debug", "test.dat");
+            string output_filename = Path.Combine(dirPath, "test.h264");
+
+            FileStream fin = File.OpenRead(input_filename);
+            FileStream fout = File.OpenWrite(output_filename);
+
+#if USE_CUDA
+            err = ffmpeg.av_hwdevice_ctx_create(&hw_device_ctx, AVHWDeviceType.AV_HWDEVICE_TYPE_CUDA, null, null, 0);
+#else
+            err = ffmpeg.av_hwdevice_ctx_create(&hw_device_ctx, AVHWDeviceType.AV_HWDEVICE_TYPE_VAAPI, null, null, 0);
+#endif
+
+            if (err < 0)
+            {
+#if USE_CUDA
+                Console.WriteLine($"Failed to create a CUDA device. Error code: {FFmpegHelper.av_err2str(err)}");
+#else
+                Console.WriteLine($"Failed to create a VAAPI device. Error code: {FFmpegHelper.av_err2str(err)}");
+#endif
+                goto close;
+            }
+
+            if ((codec = ffmpeg.avcodec_find_encoder_by_name(enc_name)) == null)
+            {
+                Console.WriteLine("Could not find encoder.");
+                err = -1;
+                goto close;
+            }
+
+            if ((avctx = ffmpeg.avcodec_alloc_context3(codec)) == null)
+            {
+                err = ffmpeg.AVERROR(ffmpeg.ENOMEM);
+                goto close;
+            }
+
+            avctx->width = width;
+            avctx->height = height;
+            avctx->time_base = new AVRational() { num = 1, den = 25 };
+            avctx->framerate = new AVRational() { num = 25, den = 1 };
+            avctx->sample_aspect_ratio = new AVRational() { num = 1, den = 1 };
+
+#if USE_CUDA
+            avctx->pix_fmt = AVPixelFormat.AV_PIX_FMT_CUDA;
+#else
+            avctx->pix_fmt = AVPixelFormat.AV_PIX_FMT_VAAPI;
+#endif
+
+            if ((err = set_hwframe_ctx(avctx, hw_device_ctx)) < 0)
+            {
+                Console.WriteLine("Failed to set hwframe context.");
+                goto close;
+            }
+
+            if ((err = ffmpeg.avcodec_open2(avctx, codec, null)) < 0)
+            {
+                Console.WriteLine($"Cannot open video encoder codec. Error code: {FFmpegHelper.av_err2str(err)}");
+                goto close;
+            }
+
+            while (true)
+            {
+                if ((sw_frame = ffmpeg.av_frame_alloc()) == null)
+                {
+                    err = ffmpeg.AVERROR(ffmpeg.ENOMEM);
+                    goto close;
+                }
+
+                sw_frame->width = width;
+                sw_frame->height = height;
+                sw_frame->format = (int)AVPixelFormat.AV_PIX_FMT_NV12;
+
+                if ((err = ffmpeg.av_frame_get_buffer(sw_frame, 0)) < 0)
+                {
+                    goto close;
+                }
+
+                Span<byte> data = new Span<byte>(sw_frame->data[0], size);
+                if ((err = fin.Read(data)) <= 0)
+                {
+                    break;
+                }
+
+                data = new Span<byte>(sw_frame->data[1], size / 2);
+                if ((err = fin.Read(data)) <= 0)
+                {
+                    break;
+                }
+
+                if ((hw_frame = ffmpeg.av_frame_alloc()) == null)
+                {
+                    err = ffmpeg.AVERROR(ffmpeg.ENOMEM);
+                    goto close;
+                }
+
+                if ((err = ffmpeg.av_hwframe_get_buffer(avctx->hw_frames_ctx, hw_frame, 0)) < 0)
+                {
+                    Console.WriteLine($"Error code: {FFmpegHelper.av_err2str(err)}");
+                    goto close;
+                }
+
+                if (hw_frame->hw_frames_ctx == null)
+                {
+                    err = ffmpeg.AVERROR(ffmpeg.ENOMEM);
+                    goto close;
+                }
+
+                if ((err = ffmpeg.av_hwframe_transfer_data(hw_frame, sw_frame, 0)) < 0)
+                {
+                    Console.WriteLine($"Error while transferring frame data to surface. Error code: {FFmpegHelper.av_err2str(err)}");
+                    goto close;
+                }
+
+                if ((err = (encode_write(avctx, hw_frame, fout))) < 0)
+                {
+                    Console.WriteLine("Failed to encode");
+                    goto close;
+                }
+
+                ffmpeg.av_frame_free(&hw_frame);
+                ffmpeg.av_frame_free(&sw_frame);
+            }
+
+            err = encode_write(avctx, null, fout);
+            if (err == ffmpeg.AVERROR_EOF)
+            {
+                Console.WriteLine($"ffplay -autoexit -f h264 -framerate 25 {output_filename}");
+                err = 0;
+            }
+
+            close:
+            if (fin != null)
+            {
+                fin.Close();
+            }
+
+            if (fout != null)
+            {
+                fout.Close();
+            }
+
+            ffmpeg.av_frame_free(&sw_frame);
+            ffmpeg.av_frame_free(&hw_frame);
+            ffmpeg.avcodec_free_context(&avctx);
+            ffmpeg.av_buffer_unref(&hw_device_ctx);
+
+            return err;
+        }
+    }
+}
---
+++ vaapi_transcode/Program.cs
@@ -0,0 +1,337 @@
+﻿using FFmpeg.AutoGen;
+using FFmpeg.AutoGen.Example;
+using System;
+using System.IO;
+
+namespace vaapi_encode
+{
+    public unsafe class Program
+    {
+        static AVFormatContext* ifmt_ctx = null;
+        static AVFormatContext* ofmt_ctx = null;
+        static AVBufferRef* hw_device_ctx = null;
+        static AVCodecContext* decoder_ctx = null;
+        static int video_stream = -1;
+        static AVStream* ost;
+        static int initialized = 0;
+
+        static unsafe AVPixelFormat get_vaapi_format(AVCodecContext* ctx, AVPixelFormat* pix_fmts)
+        {
+            AVPixelFormat* p;
+
+            for (p = pix_fmts; *p != AVPixelFormat.AV_PIX_FMT_NONE; p++)
+            {
+                if (*p == AVPixelFormat.AV_PIX_FMT_VAAPI)
+                {
+                    return *p;
+                }
+            }
+
+            Console.WriteLine("Unable to decode this file using VA-API.");
+            return AVPixelFormat.AV_PIX_FMT_NONE;
+        }
+
+        static unsafe int open_input_file(string filename)
+        {
+            int ret;
+            AVCodec* decoder = null;
+            AVStream* video = null;
+
+            fixed (AVFormatContext** pfmt_ctx = &ifmt_ctx)
+            {
+                if ((ret = ffmpeg.avformat_open_input(pfmt_ctx, filename, null, null)) < 0)
+                {
+                    Console.WriteLine($"Cannot open input file '{filename}', Error code: {FFmpegHelper.av_err2str(ret)}");
+                    return ret;
+                }
+            }
+
+            if ((ret = ffmpeg.avformat_find_stream_info(ifmt_ctx, null)) < 0)
+            {
+                Console.WriteLine($"Cannot find input stream information. Error code: {FFmpegHelper.av_err2str(ret)}");
+                return ret;
+            }
+
+            ret = ffmpeg.av_find_best_stream(ifmt_ctx, AVMediaType.AVMEDIA_TYPE_VIDEO, -1, -1, &decoder, 0);
+            if (ret < 0)
+            {
+                Console.WriteLine($"Cannot find a video stream in the input file. Error code: {FFmpegHelper.av_err2str(ret)}");
+                return ret;
+            }
+
+            video_stream = ret;
+
+            if ((decoder_ctx = ffmpeg.avcodec_alloc_context3(decoder)) == null)
+            {
+                return ffmpeg.AVERROR(ffmpeg.ENOMEM);
+            }
+
+            video = ifmt_ctx->streams[video_stream];
+            if ((ret = ffmpeg.avcodec_parameters_to_context(decoder_ctx, video->codecpar)) < 0)
+            {
+                Console.WriteLine($"{nameof(ffmpeg.avcodec_parameters_to_context)} error. Error code: {FFmpegHelper.av_err2str(ret)} ");
+                return ret;
+            }
+
+            decoder_ctx->hw_device_ctx = ffmpeg.av_buffer_ref(hw_device_ctx);
+            if (decoder_ctx->hw_device_ctx == null)
+            {
+                Console.WriteLine("A hardware device reference create failed.");
+                return ffmpeg.AVERROR(ffmpeg.ENOMEM);
+            }
+
+            decoder_ctx->get_format = (AVCodecContext_get_format_func)get_vaapi_format;
+            if ((ret = ffmpeg.avcodec_open2(decoder_ctx, decoder, null)) < 0)
+            {
+                Console.WriteLine($"Failed to open codec for decoding. Error code: {FFmpegHelper.av_err2str(ret)}");
+            }
+
+            return ret;
+        }
+
+        static unsafe int encode_write(AVCodecContext* encoder_ctx, AVPacket* enc_pkt, AVFrame* frame)
+        {
+            int ret = 0;
+
+            ffmpeg.av_packet_unref(enc_pkt);
+
+            if ((ret = ffmpeg.avcodec_send_frame(encoder_ctx, frame)) < 0)
+            {
+                Console.WriteLine($"Error during encoding. Error code: {FFmpegHelper.av_err2str(ret)}");
+                goto end;
+            }
+
+            while (true)
+            {
+                ret = ffmpeg.avcodec_receive_packet(encoder_ctx, enc_pkt);
+                if (ret != 0)
+                {
+                    break;
+                }
+
+                enc_pkt->stream_index = 0;
+                ffmpeg.av_packet_rescale_ts(enc_pkt, ifmt_ctx->streams[video_stream]->time_base, ofmt_ctx->streams[0]->time_base);
+
+                ret = ffmpeg.av_interleaved_write_frame(ofmt_ctx, enc_pkt);
+                if (ret < 0)
+                {
+                    Console.WriteLine($"Error during writing data to output file. Error code: {FFmpegHelper.av_err2str(ret)}");
+                    return -1;
+                }
+            }
+
+        end:
+            if (ret == ffmpeg.AVERROR_EOF)
+            {
+                return 0;
+            }
+
+            ret = (ret == ffmpeg.AVERROR(ffmpeg.EAGAIN)) ? 0 : -1;
+            return ret;
+        }
+
+        static unsafe int dec_enc(AVCodecContext* encoder_ctx, AVPacket* pkt, AVCodec* enc_codec)
+        {
+            AVFrame* frame;
+            int ret = ffmpeg.avcodec_send_packet(decoder_ctx, pkt);
+            if (ret < 0)
+            {
+                Console.WriteLine($"Error during decoding. Error code: {FFmpegHelper.av_err2str(ret)}");
+                return ret;
+            }
+
+            while (ret >= 0)
+            {
+                if ((frame = ffmpeg.av_frame_alloc()) == null)
+                {
+                    return ffmpeg.AVERROR(ffmpeg.ENOMEM);
+                }
+
+                ret = ffmpeg.avcodec_receive_frame(decoder_ctx, frame);
+                if (ret == ffmpeg.AVERROR(ffmpeg.EAGAIN) || ret == ffmpeg.AVERROR_EOF)
+                {
+                    ffmpeg.av_frame_free(&frame);
+                    return 0;
+                }
+                else if (ret < 0)
+                {
+                    Console.WriteLine($"Error while decoding. Error code: {FFmpegHelper.av_err2str(ret)}");
+                    goto fail;
+                }
+
+                if (initialized == 0)
+                {
+                    encoder_ctx->hw_frames_ctx = ffmpeg.av_buffer_ref(decoder_ctx->hw_frames_ctx);
+                    if (encoder_ctx->hw_frames_ctx == null)
+                    {
+                        ret = ffmpeg.AVERROR(ffmpeg.ENOMEM);
+                        goto fail;
+                    }
+
+                    encoder_ctx->time_base = FFmpegHelper.av_inv_q(decoder_ctx->framerate);
+                    encoder_ctx->pix_fmt = AVPixelFormat.AV_PIX_FMT_VAAPI;
+                    encoder_ctx->width = decoder_ctx->width;
+                    encoder_ctx->height = decoder_ctx->height;
+
+                    if ((ret = ffmpeg.avcodec_open2(encoder_ctx, enc_codec, null)) < 0)
+                    {
+                        Console.WriteLine($"Failed to open encode codec. Error code: {FFmpegHelper.av_err2str(ret)}");
+                        goto fail;
+                    }
+
+                    if ((ost = ffmpeg.avformat_new_stream(ofmt_ctx, enc_codec)) == null)
+                    {
+                        Console.WriteLine("Failed to allocate stream for output format.");
+                        ret = ffmpeg.AVERROR(ffmpeg.ENOMEM);
+                        goto fail;
+                    }
+
+                    ost->time_base = encoder_ctx->time_base;
+                    ret = ffmpeg.avcodec_parameters_from_context(ost->codecpar, encoder_ctx);
+                    if (ret < 0)
+                    {
+                        Console.WriteLine($"Failed to copy the stream parameters. Error code: {FFmpegHelper.av_err2str(ret)}");
+                        goto fail;
+                    }
+
+                    if ((ret = ffmpeg.avformat_write_header(ofmt_ctx, null)) < 0)
+                    {
+                        Console.WriteLine($"Error while writing stream header. Error code: {FFmpegHelper.av_err2str(ret)}");
+                        goto fail;
+                    }
+
+                    initialized = 1;
+                }
+
+                if ((ret = encode_write(encoder_ctx, pkt, frame)) < 0)
+                {
+                    Console.WriteLine("Error during encoding and writing");
+                }
+
+            fail:
+                ffmpeg.av_frame_free(&frame);
+                if (ret < 0)
+                {
+                    return ret;
+                }
+            }
+
+            return 0;
+        }
+
+        static unsafe int Main(string[] args)
+        {
+            FFmpegBinariesHelper.RegisterFFmpegBinaries();
+
+#if DEBUG
+            Console.WriteLine("Current directory: " + Environment.CurrentDirectory);
+            Console.WriteLine("Running in {0}-bit mode.", Environment.Is64BitProcess ? "64" : "32");
+            Console.WriteLine($"FFmpeg version info: {ffmpeg.av_version_info()}");
+            Console.WriteLine();
+#endif
+
+            AVCodec* enc_codec;
+            int ret = 0;
+            AVPacket* dec_pkt;
+            AVCodecContext* encoder_ctx = null;
+
+            string dirPath = Path.GetDirectoryName(typeof(Program).Assembly.Location) ?? "";
+            string input_filename = Path.Combine(dirPath, "..", "..", "..", "Samples", "sample-10s.mp4");
+            string encode_codec_name = "h264_nvenc";
+
+            string output_filename = Path.Combine(dirPath, "test.mp4");
+            if (File.Exists(output_filename) == true)
+            {
+                File.Delete(output_filename);
+            }
+
+            fixed (AVBufferRef** phw_device_ctx = &hw_device_ctx)
+            fixed (AVFormatContext** pofmt_ctx = &ofmt_ctx)
+            fixed (AVCodecContext** pdecoder_ctx = &decoder_ctx)
+            {
+                ret = ffmpeg.av_hwdevice_ctx_create(phw_device_ctx, AVHWDeviceType.AV_HWDEVICE_TYPE_VAAPI, null, null, 0);
+                if (ret < 0)
+                {
+                    Console.WriteLine($"Failed to create a VAAPI device. Error code: {FFmpegHelper.av_err2str(ret)}");
+                    return -1;
+                }
+
+                dec_pkt = ffmpeg.av_packet_alloc();
+                if (dec_pkt == null)
+                {
+                    Console.WriteLine("Failed to allocate decode packet");
+                    goto end;
+                }
+
+                if ((ret = open_input_file(input_filename)) < 0)
+                {
+                    goto end;
+                }
+
+                if ((enc_codec = ffmpeg.avcodec_find_encoder_by_name(encode_codec_name)) == null)
+                {
+                    Console.WriteLine($"Could not find encoder: '{encode_codec_name}'");
+                    ret = -1;
+                    goto end;
+                }
+
+                if ((ret = ffmpeg.avformat_alloc_output_context2(pofmt_ctx, null, null, output_filename)) < 0)
+                {
+                    Console.WriteLine($"Failed to deduce output format from file extension. Error code: {FFmpegHelper.av_err2str(ret)}");
+                    goto end;
+                }
+
+                if ((encoder_ctx = ffmpeg.avcodec_alloc_context3(enc_codec)) == null)
+                {
+                    ret = ffmpeg.AVERROR(ffmpeg.ENOMEM);
+                    goto end;
+                }
+
+                ret = ffmpeg.avio_open(&ofmt_ctx->pb, output_filename, ffmpeg.AVIO_FLAG_WRITE);
+                if (ret < 0)
+                {
+                    Console.WriteLine($"Cannot open output file. Error code: {FFmpegHelper.av_err2str(ret)}");
+                    goto end;
+                }
+
+                while (ret >= 0)
+                {
+                    if ((ret = ffmpeg.av_read_frame(ifmt_ctx, dec_pkt)) < 0)
+                    {
+                        break;
+                    }
+
+                    if (video_stream == dec_pkt->stream_index)
+                    {
+                        ret = dec_enc(encoder_ctx, dec_pkt, enc_codec);
+                    }
+
+                    ffmpeg.av_packet_unref(dec_pkt);
+                }
+
+                ffmpeg.av_packet_unref(dec_pkt);
+                ret = dec_enc(encoder_ctx, dec_pkt, null);
+
+                ret = encode_write(encoder_ctx, dec_pkt, null);
+
+                ffmpeg.av_write_trailer(ofmt_ctx);
+
+            end:
+                fixed (AVFormatContext** pifmt_ctx = &ifmt_ctx)
+                {
+                    ffmpeg.avformat_close_input(pifmt_ctx);
+                }
+
+                ffmpeg.avformat_close_input(pofmt_ctx);
+
+                ffmpeg.avcodec_free_context(pdecoder_ctx);
+                ffmpeg.avcodec_free_context(&encoder_ctx);
+
+                ffmpeg.av_buffer_unref(phw_device_ctx);
+                ffmpeg.av_packet_free(&dec_pkt);
+            }
+
+            return 0;
+        }
+    }
+}
